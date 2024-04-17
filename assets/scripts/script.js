1dbbaf317f6939ccd368f7814075ef89b6ff6049
ixolist-site <site@ixolist.com>
2024-04-17 17:01:17 +0300

    added tags cloud (intermediate commit)

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,9 +1,10 @@
-document.addEventListener("DOMContentLoaded", function() {
-  
+document.addEventListener("DOMContentLoaded", function() { 
   const images = document.querySelectorAll('.phone-img');
   let currentImageIndex = 0;
   images[currentImageIndex].style.display = 'flex';
   setInterval(changeImage, 4000);
+
+  tagsSizing()
 
 
   const buttons = document.querySelectorAll('.services__btn');
@@ -39,5 +40,13 @@ document.addEventListener("DOMContentLoaded", function() {
     images[currentImageIndex].style.display = 'none';
     currentImageIndex = (currentImageIndex + 1) % images.length;
     images[currentImageIndex].style.display = 'flex';
+  }
+
+  function tagsSizing() {
+    const tags = document.querySelectorAll('.tag');
+    tags.forEach(function(tag) {
+      var weight = parseInt(tag.getAttribute('data-weight'));
+      tag.style.fontSize = (10 + weight * 2) + 'px';
+    });
   }
 });
